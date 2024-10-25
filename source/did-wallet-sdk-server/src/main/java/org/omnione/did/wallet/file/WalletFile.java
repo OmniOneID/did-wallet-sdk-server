b815a3a9d4afc819a4e2d59f0ee326cb52a7ba62
sjyeo <sjyeo@raoncorp.com>
2024-10-25 14:06:07 +0900

    inital commit

--- source/did-wallet-sdk-server/src/main/java/org/omnione/did/wallet/file/WalletFile.java
+++ source/did-wallet-sdk-server/src/main/java/org/omnione/did/wallet/file/WalletFile.java
@@ -1,5 +1,17 @@
-/* 
- * Copyright 2024 Raonsecure
+/*
+ * Copyright 2024 OmniOne.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.omnione.did.wallet.file;
@@ -56,12 +68,17 @@ public class WalletFile {
 
 	/**
 	 * Delete the wallet file.
+	 * @throws WalletException 
 	 */
-	public void delete() {
+	public void delete() throws WalletException {
 		File f = new File(walletFilePath);
-		if (f.exists()) {
-			f.delete();
-		}
+        synchronized (this) {
+            if (f.exists()) {
+                f.delete();
+            } else {
+                throw new WalletException(WalletErrorCode.ERR_CODE_WALLET_NOT_EXISTS_FILE.getCode());
+            }
+        }
 	}
 	
 	 /**
